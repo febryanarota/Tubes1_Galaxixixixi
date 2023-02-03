64624dcd7863cf5ecae627c77d228d58bef463ec
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2023-02-04 00:09:49 +0700

    change bot name and add other key-values

--- src/main/java/Enums/ObjectTypes.java
+++ src/main/java/Enums/ObjectTypes.java
@@ -5,7 +5,13 @@ public enum ObjectTypes {
   FOOD(2),
   WORMHOLE(3),
   GAS_CLOUD(4),
-  ASTEROID_FIELD(5);
+  ASTEROID_FIELD(5),
+  TORPEDO_SALVO(6),
+  SUPERFOOD(7),
+  SUPERNOVA_PICKUP(8),
+  SUPERNOVA_BOMB(9),
+  TELEPORTER(10),
+  SHIELD(11);
 
   public final Integer value;
 
@@ -15,7 +21,8 @@ public enum ObjectTypes {
 
   public static ObjectTypes valueOf(Integer value) {
     for (ObjectTypes objectType : ObjectTypes.values()) {
-      if (objectType.value == value) return objectType;
+      if (objectType.value == value)
+        return objectType;
     }
 
     throw new IllegalArgumentException("Value not found");
